097393f5af43e7d05630936cb7a3c2130c95ff9e
Victor <victor.s@live.dk>
2019-02-26 10:39:47 +0100

    more experiments

--- Exercise 2 - PCA/PCA.R
+++ Exercise 2 - PCA/PCA.R
@@ -17,22 +17,25 @@ plot(cumsum(id_pca$sdev/totalvar), main="Cumulative sum of proportions of varian
 
 
 #2.1.2
-sum(id_pca$sdev[1:72]/totalvar)
+library(class)
+library(gmodels)
+library(caret)
+cumsum(id_pca$sdev/totalvar)
 
 
+indexFor80pct <- 72
+indexFor90pct <- 115
+indexFor95pct <- 157
+indexFor99pct <- 232
 
-data <- id_pca$x[,1:72]
+data <- id_pca$x[,1:indexFor80pct]
   
 train <- as.data.frame(data[0:10000,])
 test <- as.data.frame(data[10001:40000,])
 train_labels <- id_shuffle[0:10000,1]
 test_labels <- id_shuffle[10001:40000,1]
 
-library(class)
-library(gmodels)
-library(caret)
-
-"Sample size: Train 10000, Test = 30000, k = 1"
+" 80 pct k = 1"
 beforeTime <- Sys.time()
 prediction <- knn(train = train, test = test, cl = train_labels, k= 1)
 afterTime <- Sys.time()
@@ -41,7 +44,7 @@ cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
 acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
 acc
 
-"Sample size: Train 10000, Test = 30000, k = 5"
+"80 pct k = 5"
 beforeTime <- Sys.time()
 prediction <- knn(train = train, test = test, cl = train_labels, k= 5)
 afterTime <- Sys.time()
@@ -50,7 +53,7 @@ cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
 acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
 acc
 
-"Sample size: Train 10000, Test = 30000, k = 21"
+"80 pct k = 21"
 beforeTime <- Sys.time()
 prediction <- knn(train = train, test = test, cl = train_labels, k= 21)
 afterTime <- Sys.time()
@@ -58,3 +61,112 @@ afterTime-beforeTime
 cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
 acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
 acc
+
+
+
+data <- id_pca$x[,1:indexFor90pct]
+
+train <- as.data.frame(data[0:10000,])
+test <- as.data.frame(data[10001:40000,])
+train_labels <- id_shuffle[0:10000,1]
+test_labels <- id_shuffle[10001:40000,1]
+
+" 90 pct k = 1"
+beforeTime <- Sys.time()
+prediction <- knn(train = train, test = test, cl = train_labels, k= 1)
+afterTime <- Sys.time()
+afterTime-beforeTime
+cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
+acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+acc
+
+"90 pct k = 5"
+beforeTime <- Sys.time()
+prediction <- knn(train = train, test = test, cl = train_labels, k= 5)
+afterTime <- Sys.time()
+afterTime-beforeTime
+cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
+acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+acc
+
+"90 pct k = 21"
+beforeTime <- Sys.time()
+prediction <- knn(train = train, test = test, cl = train_labels, k= 21)
+afterTime <- Sys.time()
+afterTime-beforeTime
+cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
+acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+acc
+
+
+data <- id_pca$x[,1:indexFor95pct]
+
+train <- as.data.frame(data[0:10000,])
+test <- as.data.frame(data[10001:40000,])
+train_labels <- id_shuffle[0:10000,1]
+test_labels <- id_shuffle[10001:40000,1]
+
+" 95 pct k = 1"
+beforeTime <- Sys.time()
+prediction <- knn(train = train, test = test, cl = train_labels, k= 1)
+afterTime <- Sys.time()
+afterTime-beforeTime
+cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
+acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+acc
+
+"95 pct k = 5"
+beforeTime <- Sys.time()
+prediction <- knn(train = train, test = test, cl = train_labels, k= 5)
+afterTime <- Sys.time()
+afterTime-beforeTime
+cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
+acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+acc
+
+"95 pct k = 21"
+beforeTime <- Sys.time()
+prediction <- knn(train = train, test = test, cl = train_labels, k= 21)
+afterTime <- Sys.time()
+afterTime-beforeTime
+cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
+acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+acc
+
+
+
+data <- id_pca$x[,1:indexFor99pct]
+
+train <- as.data.frame(data[0:10000,])
+test <- as.data.frame(data[10001:40000,])
+train_labels <- id_shuffle[0:10000,1]
+test_labels <- id_shuffle[10001:40000,1]
+
+"99 pct k = 1"
+beforeTime <- Sys.time()
+prediction <- knn(train = train, test = test, cl = train_labels, k= 1)
+afterTime <- Sys.time()
+afterTime-beforeTime
+cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
+acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+acc
+
+"99 pct k = 5"
+beforeTime <- Sys.time()
+prediction <- knn(train = train, test = test, cl = train_labels, k= 5)
+afterTime <- Sys.time()
+afterTime-beforeTime
+cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
+acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+acc
+
+"99 pct k = 21"
+beforeTime <- Sys.time()
+prediction <- knn(train = train, test = test, cl = train_labels, k= 21)
+afterTime <- Sys.time()
+afterTime-beforeTime
+cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
+acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+acc
+
+
