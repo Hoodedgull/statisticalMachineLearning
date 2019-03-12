11f9bf390bf2045bf2cef8cff812d671657769dd
Daniel Johansen <dajoh16@student.sdu.dk>
2019-03-12 11:37:30 +0100

    Exxercises up to and including 2.3

--- Exercise 2 - PCA/PCA.R
+++ Exercise 2 - PCA/PCA.R
@@ -267,261 +267,261 @@ acc <- sum(diag(cfcMtx$table)) / sum(cfcMtx$table)
 acc
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# disjunct
+
+id_pca_disj <- prcomp(id[,-1], center = TRUE, scale = TRUE)
+
+totalvar_disj = sum(id_pca_disj$sdev)
+
+#2.1.1
+plot(id_pca_disj$sdev[1:20], main= "standard deviation (from eigenvalues)", ylab="std. dev.", xlab="Principal component")
+plot(id_pca_disj$sdev[1:20]/totalvar_disj, main = "Proportion of variance", xlab="Principal component", ylab= "proportion of total variance explained")
+plot(cumsum(id_pca_disj$sdev/totalvar), main="Cumulative sum of proportions of variance", xlab="Principal Component", ylab="cumulative sum")
+
+
+indexFor80pct <- 72
+indexFor90pct <- 115
+indexFor95pct <- 157
+indexFor99pct <- 232
+
+
+data <- id_pca_disj$x[,1:2]
+
+train <- as.data.frame(data[0:10000,])
+test <- as.data.frame(data[10001:40000,])
+train_labels <- id[0:10000,1]
+test_labels <- id[10001:40000,1]
+
+" 80 pct k = 1"
+beforeTime <- Sys.time()
+prediction <- knn(train = train, test = test, cl = train_labels, k= 1)
+afterTime <- Sys.time()
+afterTime-beforeTime
+cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
+acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+acc
+
+
+"80 pct k = 5"
+beforeTime <- Sys.time()
+prediction <- knn(train = train, test = test, cl = train_labels, k= 5)
+afterTime <- Sys.time()
+afterTime-beforeTime
+cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
+acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+acc
+
+"80 pct k = 21"
+beforeTime <- Sys.time()
+prediction <- knn(train = train, test = test, cl = train_labels, k= 21)
+afterTime <- Sys.time()
+afterTime-beforeTime
+cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
+acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+acc
+
+data <- id_pca_disj$x[,1:indexFor90pct]
+
+train <- as.data.frame(data[0:10000,])
+test <- as.data.frame(data[10001:40000,])
+train_labels <- id[0:10000,1]
+test_labels <- id[10001:40000,1]
+
+
+
+" 90 pct k = 1"
+beforeTime <- Sys.time()
+prediction <- knn(train = train, test = test, cl = train_labels, k= 1)
+afterTime <- Sys.time()
+afterTime-beforeTime
+cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
+acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+acc
+
+"90 pct k = 5"
+beforeTime <- Sys.time()
+prediction <- knn(train = train, test = test, cl = train_labels, k= 5)
+afterTime <- Sys.time()
+afterTime-beforeTime
+cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
+acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+acc
+
+"90 pct k = 21"
+beforeTime <- Sys.time()
+prediction <- knn(train = train, test = test, cl = train_labels, k= 21)
+afterTime <- Sys.time()
+afterTime-beforeTime
+cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
+acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+acc
+
+data <- id_pca_disj$x[,1:indexFor95pct]
+
+train <- as.data.frame(data[0:10000,])
+test <- as.data.frame(data[10001:40000,])
+train_labels <- id[0:10000,1]
+test_labels <- id[10001:40000,1]
+
+
+
+" 95 pct k = 1"
+beforeTime <- Sys.time()
+prediction <- knn(train = train, test = test, cl = train_labels, k= 1)
+afterTime <- Sys.time()
+afterTime-beforeTime
+cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
+acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+acc
+
+"95 pct k = 5"
+beforeTime <- Sys.time()
+prediction <- knn(train = train, test = test, cl = train_labels, k= 5)
+afterTime <- Sys.time()
+afterTime-beforeTime
+cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
+acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+acc
+
+"95 pct k = 21"
+beforeTime <- Sys.time()
+prediction <- knn(train = train, test = test, cl = train_labels, k= 21)
+afterTime <- Sys.time()
+afterTime-beforeTime
+cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
+acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+acc
+
+data <- id_pca_disj$x[,1:indexFor99pct]
+
+train <- as.data.frame(data[0:10000,])
+test <- as.data.frame(data[10001:40000,])
+train_labels <- id[0:10000,1]
+test_labels <- id[10001:40000,1]
+
+"99 pct k = 1"
+beforeTime <- Sys.time()
+prediction <- knn(train = train, test = test, cl = train_labels, k= 1)
+afterTime <- Sys.time()
+afterTime-beforeTime
+cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
+acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+acc
+
+"99 pct k = 5"
+beforeTime <- Sys.time()
+prediction <- knn(train = train, test = test, cl = train_labels, k= 5)
+afterTime <- Sys.time()
+afterTime-beforeTime
+cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
+acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+acc
+
+"99 pct k = 21"
+beforeTime <- Sys.time()
+prediction <- knn(train = train, test = test, cl = train_labels, k= 21)
+afterTime <- Sys.time()
+afterTime-beforeTime
+cfcMtx <- confusionMatrix(data = prediction, reference = test_labels)
+acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+acc
+
+# 2.2
+
+normalize <- function(x){ 
+  return ((x-min(x))/(max(x)-min(x)))
+  
+}
+
+#No normalization
+
+id_pca <- prcomp(id_shuffle[,-1], center = TRUE) # maybe remove scale=TRUE
+data <- id_pca$x[,1:indexFor80pct]
+
+
+folds <- createFolds(id_shuffle$V1, k = 10)
+
+results = c()
+for(i in 1:10){
+  train <- data[-folds[[i]],-1]
+  test <- data[folds[[i]],-1]
+  
+  train_labels <- id_shuffle[-folds[[i]],1] 
+  test_labels <- id_shuffle[folds[[i]],1] 
+  
+  result <- knn(train = train, test = test, cl = train_labels, k = 1)
+  cfcMtx <- confusionMatrix(data = result, reference = test_labels)
+  acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+  results <- c(results, acc)
+}
+#No normalization
+summary(results)
+sd(results)
+
+#BEFORE PCA normalization
+id_shuffle_norm <- lapply(id_shuffle[,-1],normalize)
+id_shuffle_norm <- as.data.frame(id_shuffle_norm)
+id_pca <- prcomp(id_shuffle_norm[], center = TRUE) # maybe remove scale=TRUE
+data <- id_pca$x[,1:indexFor80pct]
+
+
+folds <- createFolds(id_shuffle$V1, k = 10)
+
+results = c()
+for(i in 1:10){
+  train <- data[-folds[[i]],-1]
+  test <- data[folds[[i]],-1]
+  
+  train_labels <- id_shuffle[-folds[[i]],1] 
+  test_labels <- id_shuffle[folds[[i]],1] 
+  
+  result <- knn(train = train, test = test, cl = train_labels, k = 1)
+  cfcMtx <- confusionMatrix(data = result, reference = test_labels)
+  acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+  results <- c(results, acc)
+}
+#BEFORE PCA normalization
+summary(results)
+sd(results)
+
+#AFTER PCA normalization
+id_pca <- prcomp(id_shuffle[,-1], center = TRUE) # maybe remove scale=TRUE
+data <- id_pca$x[,1:indexFor80pct]
+data <- as.data.frame(data)
+data <- lapply(data,normalize)
+data <- as.data.frame(data)
+
+folds <- createFolds(id_shuffle$V1, k = 10)
+
+results = c()
+for(i in 1:10){
+  train <- data[-folds[[i]],-1]
+  test <- data[folds[[i]],-1]
+  
+  train_labels <- id_shuffle[-folds[[i]],1] 
+  test_labels <- id_shuffle[folds[[i]],1] 
+  
+  result <- knn(train = train, test = test, cl = train_labels, k = 1)
+  cfcMtx <- confusionMatrix(data = result, reference = test_labels)
+  acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+  results <- c(results, acc)
+}
+#AFTER PCA normalization
+summary(results)
+sd(results)
 
 
 #2.3 Preprocessing
+#Setup
 library(spatstat)
+id_1Person <- id[0:12000,]
 
-id_mat <- data.matrix(id, rownames.force = NA)
+id_mat <- data.matrix(id_1Person, rownames.force = NA)
 imageSize <- sqrt(ncol(id_mat) - 1)
 rotate <- function(x)
   t(apply(x, 2, rev))
 
+#Gaussian Smoothing Sigma 0.5
 smoothImage <- function(grayImg) {
   smoothed <-
     as.matrix(blur(
@@ -533,6 +533,8 @@ smoothImage <- function(grayImg) {
     ))
   return(smoothed)
 }
+
+#Gaussian Smoothing 
 # Smooth all images
 for (i in 1:nrow(id_mat))
 {
@@ -549,5 +551,148 @@ for (i in 1:nrow(id_mat))
            ncol = ncol(id_mat) - 1,
            byrow = FALSE)
 }
-id <- as.data.frame(id_mat)
-id[, 1] <- factor(id[, 1])
+id_100 <- as.data.frame(id_mat)
+id_100[, 1] <- factor(id_100[, 1])
+id_100 <- id_100[sample(nrow(id_100)), ]
+folds <- createFolds(id_100$V1, k = 10)
+data <- id_100[,]
+results = c()
+for(i in 1:10){
+  train <- data[-folds[[i]],-1]
+  test <- data[folds[[i]],-1]
+  
+  train_labels <- id_100[-folds[[i]],1] 
+  test_labels <- id_100[folds[[i]],1] 
+  
+  result <- knn(train = train, test = test, cl = train_labels, k = 1)
+  cfcMtx <- confusionMatrix(data = result, reference = test_labels)
+  acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+  results <- c(results, acc)
+}
+#Sigma 0.5
+summary(results)
+sd(results)
+
+
+
+
+
+#Gaussian Smoothing Sigma 0.25
+id_1Person <- id[0:12000,]
+
+id_mat <- data.matrix(id_1Person, rownames.force = NA)
+imageSize <- sqrt(ncol(id_mat) - 1)
+rotate <- function(x)
+  t(apply(x, 2, rev))
+
+smoothImage <- function(grayImg) {
+  smoothed <-
+    as.matrix(blur(
+      as.im(grayImg),
+      sigma = 0.25,
+      normalise = FALSE,
+      bleed = TRUE,
+      varcov = NULL
+    ))
+  return(smoothed)
+}
+
+#Gaussian Smoothing 
+# Smooth all images
+for (i in 1:nrow(id_mat))
+{
+  rotated <- c(id_mat[i, 2:ncol(id)])
+  image <-
+    matrix(rotated,
+           nrow = imageSize,
+           ncol = imageSize,
+           byrow = FALSE)
+  image <- smoothImage(image)
+  id_mat[i, 2:ncol(id_mat)] <-
+    matrix(image,
+           nrow = 1,
+           ncol = ncol(id_mat) - 1,
+           byrow = FALSE)
+}
+id_100 <- as.data.frame(id_mat)
+id_100[, 1] <- factor(id_100[, 1])
+id_100 <- id_100[sample(nrow(id_100)), ]
+
+folds <- createFolds(id_100$V1, k = 10)
+data <- id_100[,]
+results = c()
+for(i in 1:10){
+  train <- data[-folds[[i]],-1]
+  test <- data[folds[[i]],-1]
+  
+  train_labels <- id_100[-folds[[i]],1] 
+  test_labels <- id_100[folds[[i]],1] 
+  
+  result <- knn(train = train, test = test, cl = train_labels, k = 1)
+  cfcMtx <- confusionMatrix(data = result, reference = test_labels)
+  acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+  results <- c(results, acc)
+}
+#Sigma 0.25
+summary(results)
+sd(results)
+
+#Gaussian Smoothing Sigma 0.75
+id_1Person <- id[0:12000,]
+
+id_mat <- data.matrix(id_1Person, rownames.force = NA)
+imageSize <- sqrt(ncol(id_mat) - 1)
+rotate <- function(x)
+  t(apply(x, 2, rev))
+
+smoothImage <- function(grayImg) {
+  smoothed <-
+    as.matrix(blur(
+      as.im(grayImg),
+      sigma = 0.75,
+      normalise = FALSE,
+      bleed = TRUE,
+      varcov = NULL
+    ))
+  return(smoothed)
+}
+
+#Gaussian Smoothing 
+# Smooth all images
+for (i in 1:nrow(id_mat))
+{
+  rotated <- c(id_mat[i, 2:ncol(id)])
+  image <-
+    matrix(rotated,
+           nrow = imageSize,
+           ncol = imageSize,
+           byrow = FALSE)
+  image <- smoothImage(image)
+  id_mat[i, 2:ncol(id_mat)] <-
+    matrix(image,
+           nrow = 1,
+           ncol = ncol(id_mat) - 1,
+           byrow = FALSE)
+}
+id_100 <- as.data.frame(id_mat)
+id_100[, 1] <- factor(id_100[, 1])
+id_100 <- id_100[sample(nrow(id_100)), ]
+folds <- createFolds(id_100$V1, k = 10)
+data <- id_100[,]
+results = c()
+for(i in 1:10){
+  train <- data[-folds[[i]],-1]
+  test <- data[folds[[i]],-1]
+  
+  train_labels <- id_100[-folds[[i]],1] 
+  test_labels <- id_100[folds[[i]],1] 
+  
+  result <- knn(train = train, test = test, cl = train_labels, k = 1)
+  cfcMtx <- confusionMatrix(data = result, reference = test_labels)
+  acc <- sum(diag(cfcMtx$table))/sum(cfcMtx$table)
+  results <- c(results, acc)
+}
+#Sigma 0.75
+summary(results)
+sd(results)
+
