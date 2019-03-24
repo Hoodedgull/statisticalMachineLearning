a024fc9af4fd8184042075d31f3a5e0053d5b997
Vic <victor.s@live.dk>
2019-03-24 15:49:20 +0100

    Exerise 3.3 still WIP

--- exercise 3 - clustering/script.R
+++ exercise 3 - clustering/script.R
@@ -1,7 +1,7 @@
 library(class)
 library(gmodels)
 library(caret)
-load("idList-corner-300.Rdata")
+load("idList-cornered-100.Rdata")
 id <- idList[15:16]
 trainDataSet <- as.data.frame(id[1])
 
@@ -14,7 +14,7 @@ test_set <- testDataSet[ 1 : nrow(testDataSet) ,2:ncol(testDataSet)]
 test_labels <- testDataSet[  1 : nrow(testDataSet) ,1]
 
 # id_mat <- data.matrix(testDataSet, rownames.force = NA)
-# 
+
 # rotate <- function(x)
 #   t(apply(x, 2, rev))
 # imageSize <- sqrt(ncol(id_mat) - 1)
@@ -130,6 +130,43 @@ plot(dendr,labels=train_lab)
 
 
 #3.3
+id <- do.call(rbind, idList[15:17])
+id <- as.data.frame(id)
+id$V1 <- factor(id$V1)
+
+#ALLLLL Persons in!
+id_shuffle <- id[sample(nrow(id)),]
+train <- id_shuffle[0:3000,-1]
+test <- id_shuffle[3001:6000,-1]
+train_labels <- id_shuffle[0:3000,1]
+test_labels <- id_shuffle[3001:6000,1]
+
+# https://stackoverflow.com/a/36843900
+f1_score <- function(predicted, expected) {
+  # Hacky hack to ensure that we always have at least one NA
+  # So we get a square confusion matrix
+  predicted[3000] <- NA
+  expected[3000] <- NA
+  
+  predicted <- factor(predicted, exclude=NULL)
+  expected  <- factor(expected, exclude=NULL)
+  addNA(expected)
+  addNA(predicted)
+  
+  cm <- as.matrix(table(expected, predicted, exclude=NULL))
+  print(cm)
+  precision <- diag(cm) / colSums(cm)
+  recall <- diag(cm) / rowSums(cm)
+  f1 <-  ifelse(precision + recall == 0, 0, 2 * precision * recall / (precision + recall))
+  
+  # We get a precision, recall and f1-score for each factor level
+  # So i guess we just avg them? Seems fine to me.
+  return(c(mean(precision),mean(recall),mean(f1)))
+  
+}
+
+
+
 accuracydata <- c()
 precisondata <- c()
 recalldata <- c()
@@ -139,12 +176,40 @@ f1score <- c()
 for(i in 0:6){
   k <- i*2+1 
   for(j in 1:k){
-    predictions <- knn( ??????????? k =k, l = j)
-    
+    predictions <- knn(train = train, test = test, cl = train_labels, k =k, l = j)
+    summary(predictions)
+    summary(test_labels)
     #calc prec and recall
-    
+    scores <- f1_score(predictions, test_labels)
     #store data in data arrays
+    precisondata <- c(precisondata, scores[1])
+    recalldata <- c(recalldata, scores[2])
+    f1score <- c(f1score, scores[3])
+    kdata <- c(kdata,k)
+    ldata <- c(ldata,j)
+    print(c(k,j))
   }
 }
 
 # plot deliciuous data
+plot(  recalldata)
+plot(x=kdata,  y=recalldata)
+plot(  precisondata)
+plot(x=recalldata,  y=precisondata)
+?plot
+plot(0,0,xlim = c(0,1),ylim = c(0.8,1),type = "n",xlab = "recall", ylab = "precision")
+
+cl <- rainbow(6)
+prev<-0
+for(i in 1:6){
+ 
+  k <- i*2+1
+  start <- prev+1
+  end <- prev+k
+  recData <- recalldata[start:end]
+  precData <- precisondata[start:end]
+  lines(x=recData,  y=precData,col = cl[i],type = 'b')
+  prev <- prev + k
+}
+legend("topleft", legend = c(3,5,7,9,11,13), col=cl, pch=1)
+
