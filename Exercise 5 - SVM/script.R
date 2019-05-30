14f2e2de7b2ea6fe76acc96f0a24a37e694824d0
Markus <mabar16@student.sdu.dk>
2019-05-30 16:18:20 +0200

    All-in

--- Exercise 5 - SVM/script.R
+++ Exercise 5 - SVM/script.R
@@ -71,13 +71,13 @@ kernelModes <- c("rbfdot", "vanilladot", "polydot")
 
 set.seed(2345)
 
-folds <- createFolds(dataset$V1, k = 10)
+folds <- createFolds(dataset$V1, k = 5)
 
 
 for (kernelMode in kernelModes) {
   
   for (index in 1:10) {
-    cValue <- index * 2
+    cValue <- index * 100
     
     resultsList <- c()
     TreeRuntimeList <- c()
@@ -133,6 +133,89 @@ for (kernelMode in kernelModes) {
     print(c("The C value: ", cValue))
     
     print(c("The kernel mode: ", kernelMode))
+    
+    
+    #Mean
+    print(c("The mean ", mean(resultsList)))
+    
+    print(c("The Standard Deviation ", sd(resultsList)))
+    
+    
+    print(c(
+      "The Support Vector Model Training runtime ",
+      mean(TreeRuntimeList)
+    ))
+    
+    
+    print(c("The Prediction runtime ", mean(PredictionRuntimeList)))
+    
+    print("############################################")
+    
+  }
+}
+
+degree <- c(1, 2, 3, 4, 5, 10)
+scales <- c(1, 2, 3, 4, 5, 6)
+offset <- c(0, 1, 2, 3, 4, 5)
+for (x in 1:6) {
+  for (index in 1:10) {
+    cValue <- index * 0.5
+    
+    resultsList <- c()
+    TreeRuntimeList <- c()
+    PredictionRuntimeList <- c()
+    
+    for (i in 1:5) {
+      #Without headers
+      data_test <- dataset[folds[[i]],-1]
+      data_train <- dataset[-folds[[i]],-1]
+      
+      #Get the labels
+      data_test_labels <-
+        dataset[folds[[i]], 1] #Dataset is set in both AllPersonIn and Disjunct
+      
+      #Get the labels
+      data_train_labels <- dataset[-folds[[i]], 1]
+      
+      ####Compute Decisiont Tree Runtime ############################
+      form <- as.formula(data_train_labels ~ .)
+      
+      startTime <- proc.time()
+      arguments <- list(scales[x])
+      SVM_model <-
+        ksvm(form, data_train, kernel = 'polydot', kpar = arguments, C = cValue)
+      
+      computationTime <<- proc.time() - startTime
+      
+      TreeRuntimeList[i] <- computationTime
+      
+      #######################################
+      
+      ####Compute Prediction runtime ############################
+      startTime <- proc.time()
+      
+      data_test_pred <- predict(SVM_model, data_test)
+      
+      computationTime <<- proc.time() - startTime
+      
+      PredictionRuntimeList[i] <- computationTime
+      
+      #######################################
+      
+      # Matrix
+      cf <- confusionMatrix(data_test_labels, data_test_pred)
+      
+      print(c("Precision: ", i , sum(diag(
+        cf$table / sum(cf$table)
+      ))))
+      
+      resultsList[i] <- sum(diag(cf$table / sum(cf$table)))
+    }
+    
+    print(c("The C value: ", cValue))
+    
+    print(c("The kernel paramters: ", arguments))
+    print(c("Numbers: ", x))
     
     
     #Mean
