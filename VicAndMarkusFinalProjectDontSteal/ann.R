3291d7cc3120014b3a960533564c3231827f8901
Victor <victor.s@live.dk>
2019-05-31 11:12:02 +0200

    annn

--- VicAndMarkusFinalProjectDontSteal/ann.R
+++ VicAndMarkusFinalProjectDontSteal/ann.R
@@ -3,68 +3,196 @@ library(RSNNS) #For nn
 
 load("idList-cornered-100.Rdata")
 data <- as.data.frame(idList[1])
-for(i in 2:79){
+for (i in 2:79) {
   aPerson <- as.data.frame(idList[i])
-  data <- rbind(data,aPerson)
+  data <- rbind(data, aPerson)
   
 }
 
-measure_acc <- function(true, predicted){
+measure_acc <- function(true, predicted) {
   count = 0
-  for( i in 1:length(true)){
-    if(true[i] == predicted[i]){
-      count <- count +1
+  for (i in 1:length(true)) {
+    if (true[i] == predicted[i]) {
+      count <- count + 1
     }
   }
   
-  return(count/length(true))
+  return(count / length(true))
   
 }
 
-train_data <- data[1:(5*2000),-1]
-train_labels <- data[1:(5*2000),1]
-test_data <- data[(5*2000+1):(10*2000),-1]
-test_labels<- data[(5*2000+1):(10*2000),1]
+nnacc_from_william <- function(predictions, test_labels){
+  
+  ################The bottom part of williams stuff
+  responselist <-
+    matrix(nrow = length(predictions[, 1]),
+           ncol = 1,
+           data = "Na")
+  
+  for (i in 1:nrow(predictions)) {
+    responselist[i, ] <-
+      toString(which(predictions[i, ] == max(predictions[i, ])) - 1)
+  }
+  responselist <- data.frame(responselist)
+  responselist[, 1] <- as.factor(responselist[, 1])
+  
+  # Calculating the accuracy
+  agreement_rbf <- responselist[, 1] == test_labels
+  table(agreement_rbf)
+  acc <- prop.table(table(agreement_rbf))[2]
+  return(acc)
+}
 
-?rsnns.learnFuncParams
-?rsnnsObjectFactory
+train_data <- data[(10 * 2000 + 1):(15 * 2000), -1]
+train_labels <- data[(10 * 2000 + 1):(15 * 2000), 1]
+test_data <- data[(15 * 2000 + 1):(20 * 2000), -1]
+test_labels <- data[(15 * 2000 + 1):(20 * 2000), 1]
 
 
 ################## The top part of williams stuff
 lev <- levels(as.factor(data$X1)) # Number of classes?levels
 
-nnTrainingClass <- matrix(nrow = length(data$X1), ncol = 10, data = 0) # Create a list probabilities, for all labels
+nnTrainingClass <-
+  matrix(nrow = length(data$X1),
+         ncol = 10,
+         data = 0) # Create a list probabilities, for all labels
 
-for(i in 1:length(data$X1)) { # Set probabilities to one for matching class
-  matchList <- match(lev,toString(data$X1[i]))
+for (i in 1:length(data$X1)) {
+  # Set probabilities to one for matching class
+  matchList <- match(lev, toString(data$X1[i]))
   matchList[is.na(matchList)] <- 0
-  nnTrainingClass[i,] <- matchList
+  nnTrainingClass[i, ] <- matchList
 }
 
 trainingClass <- as.data.frame(nnTrainingClass)
-summary(trainingClass[1,])
+summary(trainingClass[1, ])
 ######################### Real stuff
-nn <- mlp(x=train_data, y=trainingClass[1:(5*2000),], size = c(5), maxit = 500,
-          initFunc = "Randomize_Weights", initFuncParams = c(-0.3, 0.3),
-          learnFunc = "Std_Backpropagation", learnFuncParams = c(0.5, 0),
-          updateFunc = "Topological_Order", updateFuncParams = c(0),
-          hiddenActFunc = "Act_Logistic", shufflePatterns = TRUE, linOut = FALSE,
-          inputsTest = NULL, targetsTest = NULL, pruneFunc = NULL,
-          pruneFuncParams = NULL)
-predictions <- predict(nn, test_data)
-summary(predictions)
 
-################The bottom part of williams stuff
-responselist <- matrix(nrow = length(predictions[,1]), ncol = 1, data = "Na")
-
-for(i in 1:nrow(predictions)) {
-  responselist[i,] <- toString( which(predictions[i,]==max(predictions[i,])) - 1 )
+###################################################################Find best num of neurons
+acc_list <- c()
+tacc_list <- c()
+ttime_list <- c()
+ptime_list <- c()
+for (neurons in c(1, 5, 10, 25, 50, 100)) {
+ tic("train")
+   nn <-
+    mlp(
+      x = train_data,
+      y = trainingClass[(15 * 2000 + 1):(20 * 2000), ],
+      size = c(neurons), 
+      maxit = 100,
+      initFunc = "Randomize_Weights",
+      initFuncParams = c(-0.3, 0.3),
+      learnFunc = "Std_Backpropagation",
+      learnFuncParams = c(0.01, 0),
+      updateFunc = "Topological_Order",
+      updateFuncParams = c(0),
+      hiddenActFunc = "Act_Logistic",
+      shufflePatterns = TRUE,
+      linOut = FALSE,
+      inputsTest = NULL,
+      targetsTest = NULL,
+      pruneFunc = NULL,
+      pruneFuncParams = NULL
+    )
+  ttime <-  toc()
+  ttime_list <- c(ttime_list,ttime)
+  tic("predict")
+  predictions <- predict(nn, test_data)
+  ptime <- toc()
+  ptime_list <- c(ptime_list,ptime)
+  
+  acc <- nnacc_from_william(predictions, test_labels)
+  acc_list <- c(acc_list,acc)
+  
+  tpredictions <- predict(nn, train_data)
+  tacc <- nnacc_from_william(tpredictions, train_labels)
+  tacc_list <- c(tacc_list,tacc)
 }
-responselist <- data.frame(responselist)
-responselist[,1] <- as.factor(responselist[,1])
 
-# Calculating the accuracy
-agreement_rbf <- responselist[,1] == test_labels
-table(agreement_rbf)
-prop.table(table(agreement_rbf))
+###################################################################Find best num of epochs
 
+acc_list2 <- c()
+tacc_list2 <- c()
+ttime_list2 <- c()
+ptime_list2 <- c()
+for (epochs in c(1, 5, 10, 25, 50, 100, 200, 500, 1000)) {
+  tic("train")
+  nn <-
+    mlp(
+      x = train_data,
+      y = trainingClass[(15 * 2000 + 1):(20 * 2000), ],
+      size = c(50), 
+      maxit = epochs,
+      initFunc = "Randomize_Weights",
+      initFuncParams = c(-0.3, 0.3),
+      learnFunc = "Std_Backpropagation",
+      learnFuncParams = c(0.01, 0),
+      updateFunc = "Topological_Order",
+      updateFuncParams = c(0),
+      hiddenActFunc = "Act_Logistic",
+      shufflePatterns = TRUE,
+      linOut = FALSE,
+      inputsTest = NULL,
+      targetsTest = NULL,
+      pruneFunc = NULL,
+      pruneFuncParams = NULL
+    )
+  ttime <-  toc()
+  ttime_list2 <- c(ttime_list,ttime)
+  tic("predict")
+  predictions <- predict(nn, test_data)
+  ptime <- toc()
+  ptime_list2 <- c(ptime_list,ptime)
+  
+  acc <- nnacc_from_william(predictions, test_labels)
+  acc_list2 <- c(acc_list,acc)
+  
+  tpredictions <- predict(nn, train_data)
+  tacc <- nnacc_from_william(tpredictions, train_labels)
+  tacc_list2 <- c(tacc_list,tacc)
+}
+
+
+###################################################################Find best lr
+
+acc_list3 <- c()
+tacc_list3 <- c()
+ttime_list3 <- c()
+ptime_list3 <- c()
+for (lr in c(0.0001, 0.001, 0.01,0,05, 0.1,0.25, 0.5, 0.75, 1,2)) {
+  tic("train")
+  nn <-
+    mlp(
+      x = train_data,
+      y = trainingClass[(15 * 2000 + 1):(20 * 2000), ],
+      size = c(50), 
+      maxit = epochs,
+      initFunc = "Randomize_Weights",
+      initFuncParams = c(-0.3, 0.3),
+      learnFunc = "Std_Backpropagation",
+      learnFuncParams = c(0.01, 0),
+      updateFunc = "Topological_Order",
+      updateFuncParams = c(0),
+      hiddenActFunc = "Act_Logistic",
+      shufflePatterns = TRUE,
+      linOut = FALSE,
+      inputsTest = NULL,
+      targetsTest = NULL,
+      pruneFunc = NULL,
+      pruneFuncParams = NULL
+    )
+  ttime <-  toc()
+  ttime_list3 <- c(ttime_list,ttime)
+  tic("predict")
+  predictions <- predict(nn, test_data)
+  ptime <- toc()
+  ptime_list3 <- c(ptime_list,ptime)
+  
+  acc <- nnacc_from_william(predictions, test_labels)
+  acc_list3 <- c(acc_list,acc)
+  
+  tpredictions <- predict(nn, train_data)
+  tacc <- nnacc_from_william(tpredictions, train_labels)
+  tacc_list3 <- c(tacc_list,tacc)
+}
--- VicAndMarkusFinalProjectDontSteal/knn.R
+++ VicAndMarkusFinalProjectDontSteal/knn.R
@@ -1,5 +1,6 @@
 library(class) #For knn
 library(tictoc) #For timing
+library(caret) #For k-folds
 
 load("idList-cornered-100.Rdata")
 data <- as.data.frame(idList[1])
@@ -21,26 +22,29 @@ measure_acc <- function(true, predicted){
   
 }
 
-train_data <- data[1:(5*2000),-1]
-train_labels <- data[1:(5*2000),1]
-test_data <- data[(5*2000+1):(10*2000),-1]
-test_labels<- data[(5*2000+1):(10*2000),1]
+train_data <- data[(10*2000+1):(15*2000),-1]
+train_labels <- data[(10*2000+1):(15*2000),1]
+test_data <- data[(15*2000+1):(20*2000),-1]
+test_labels<- data[(15*2000+1):(20*2000),1]
 
 #Find best k
 acc_list <- c()
 time_list <- c()
-for( k in c(1, 2, 3, 5, 7, 10, 15, 20, 30, 50, 75, 100, 150, 200)){
+for( k in c(1,2,3,5,7,11,21,31,51,75, 101, 151, 201,503)){
   
 
 tic("knn")
-predicted_labels <- knn(train=train_data, test = test_data, cl = train_labels, k = 3)
+predicted_labels <- knn(train=train_data, test = test_data, cl = train_labels, k = k)
 time <- toc(quiet=TRUE)
 acc <- measure_acc(test_labels,predicted_labels)
 acc_list <- c(acc_list,acc)
 time_list <- c(time_list, time$toc-time$tic)
 }
 
-
+plot(x=c(1,2,3,5,7,11,21,31,51,75, 101, 151, 201,503), y=c(acc_list), type = "o")
+plot(x=c(1, 2, 3, 5, 7, 10, 15, 20, 30, 50, 75, 100, 150, 200), y=c(time_list))
+lapply(acc_list,print)
+lapply(time_list,print)
 tic("pca")
 pca_result <- prcomp(data[,-1], center = TRUE, scale = TRUE)
 toc()
@@ -63,15 +67,18 @@ find_index_for_a_pct_of_variance <- function(pca, variance_pct){
 #Find best pca
 acc_list <- c()
 time_list <- c()
-for( var in c(10,20,30,40,50,60,70,80,90,99)){
+for( var in c(10,20,30,40,50,60,70,80,90,99,100)){
   index <- find_index_for_a_pct_of_variance(pca_result, var)
-  train_data_pca <- pca_result$x[1:(5*2000),1:index]
-  test_data_pca <- pca_result$x[((5*2000)+1):(10*2000),1:index]
+  train_data_pca <- pca_result$x[(10*2000+1):(15*2000),1:index]
+  test_data_pca <- pca_result$x[((15*2000)+1):(20*2000),1:index]
   
   tic("knn")
-  predicted_labels <- knn(train=train_data_pca, test = test_data_pca, cl = train_labels, k = somevaluek)
+  predicted_labels <- knn(train=train_data_pca, test = test_data_pca, cl = train_labels, k = 3)
   time <- toc(quiet=TRUE)
   acc <- measure_acc(test_labels,predicted_labels)
   acc_list <- c(acc_list,acc)
   time_list <- c(time_list, time$toc-time$tic)
 }
+
+
+plot(x=c(10,20,30,40,50,60,70,80,90,99,100), y=c(acc_list), type = "o")
